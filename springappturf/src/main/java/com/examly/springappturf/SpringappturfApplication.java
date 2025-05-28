8f7443481956086e9c56ca4f2793d864694937b5
neojarvis <techadmin@iamneo.ai>
2025-05-28 04:49:57 +0000

    initialCommit-Boilerplate

---
+++ springappturf/src/main/java/com/examly/springappturf/SpringappturfApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springappturf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappturfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappturfApplication.class, args);
+	}
+
+}
