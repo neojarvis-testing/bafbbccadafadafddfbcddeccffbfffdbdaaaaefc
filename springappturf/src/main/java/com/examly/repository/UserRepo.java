f08bd8cfe501ce37ab86722ac4f0ecff8b9c454e
neojarvis <techadmin@iamneo.ai>
2025-05-28 14:11:25 +0000

    Manual-save-commit-5/28/2025--7:41:20-PM

--- springappturf/src/main/java/com/examly/repository/UserRepo.java
+++ springappturf/src/main/java/com/examly/repository/UserRepo.java
@@ -1,8 +1,15 @@
 package main.java.com.examly.repository;
 
-@repository
+import java.util.Optional;
+
+import main.java.com.examly.model.User;
+
+@Repository
 public interface UserRepo extends JpaRepository<User, String>{
-    
-}
+
+    @Query("select u from User u where u.email=:email where u.password=:password")
+    public User validate(@Param ("email") String email,@Param("password") String password);
+
+    }
 
 
