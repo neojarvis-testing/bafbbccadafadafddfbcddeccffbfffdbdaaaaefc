0817bc845119538297d18f6d0a25adf1594862a5
neojarvis <techadmin@iamneo.ai>
2025-05-29 09:26:12 +0000

    Auto-save-commit-5/29/2025--2:56:09-PM

--- springappturf/src/main/java/com/examly/services/TurfService.java
+++ springappturf/src/main/java/com/examly/services/TurfService.java
@@ -2,12 +2,14 @@ package com.examly.services;
 
 import java.util.List;
 
-vice  {
+import com.examly.model.Turf;
+
+public interface TurfService {
 
     public List<Turf> getTurfs();
     public void addTurf(Turf t);
     public void editTurf(Turf t);
-    public List<Turf> getTurfById(int turfId);
+    public Turf getTurfById(int turfId);
     public void deleteTurf(int turfId);
 
 }
--- springappturf/src/main/java/com/examly/services/TurfServiceImpl.java
+++ springappturf/src/main/java/com/examly/services/TurfServiceImpl.java
@@ -1,14 +1,37 @@
 package com.examly.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.examly.model.Turf;
 import com.examly.repository.TurfRepo;
 
 @Service
 public class TurfServiceImpl implements TurfService{
     @Autowired
     TurfRepo repo;
-    
+     public List<Turf> getTurfs(){
+        return repo.findAll();
+
+     }
+    public void addTurf(Turf t){
+        repo.save(t);
+
+    }
+    public void editTurf(Turf t){
+        repo.save(t);
+
+    }
+    public Turf getTurfById(int turfId){
+        if(repo.existsById(turfId))
+
+
+    }
+    public void deleteTurf(int turfId){
+
+    }
+
 
 }
