a9bbd426063eb946b9710d764e2992e02dbcb334
neojarvis <techadmin@iamneo.ai>
2025-05-29 11:11:11 +0000

    Auto-save-commit-5/29/2025--4:41:09-PM

--- springappturf/src/main/java/com/examly/controller/UserController.java
+++ springappturf/src/main/java/com/examly/controller/UserController.java
@@ -2,10 +2,12 @@ package com.examly.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examly.ExceptionHandling.TurfExceptions;
+import com.examly.model.User;
 import com.examly.services.UserServiceImpl;
 
 @RestController
@@ -20,7 +22,10 @@ public class UserController {
         return "Login Success";
     }
 
-    public User addUser()
+    public User addUser(@RequestBody User u){
+        service.addUser(u);
+        return u;
+    }
 
 
 }
